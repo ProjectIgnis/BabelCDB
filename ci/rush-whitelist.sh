337782b46b099f8f629abbe6d9f6a708f99d3446
Edoardo Lolletti <edoardo762@gmail.com>
2023-01-05 17:58:27 +0100

    Update rush banlist generation
    Don't put legends limited at anymore

--- ci/rush-whitelist.sh
+++ ci/rush-whitelist.sh
@@ -13,9 +13,6 @@ function write_header {
 	echo "#[$3 $1]" > $2
 	echo "!$3 $1" >> $2
 	echo "\$whitelist" >> $2
-	echo "160000000 1 --Legend" >> $2
-	# All Legend cards must be aliased to this ID and hold the 0x400 scope flag.
-	# They are ignored by this script. This is a workaround.
 }
 
 write_header "Rush Duel" $OUTPUT_OFFICIAL $DATE_OFFICIAL
@@ -35,18 +32,12 @@ echo "# Legends" >> $OUTPUT_OFFICIAL
 echo "# Legends" >> $OUTPUT_PRERELEASE
 
 find . -type f -name '*.cdb' -exec sqlite3 {} \
-    "SELECT (datas.id || ' ' || 1 || ' --' || name) FROM datas INNER JOIN texts ON datas.id=texts.id WHERE ot == 0x200|0x400 ORDER BY datas.id" \; \
+    "SELECT (datas.id || ' ' || 3 || ' --' || name) FROM datas INNER JOIN texts ON datas.id=texts.id WHERE ot == 0x200|0x400 ORDER BY datas.id" \; \
     | tee -a $OUTPUT_OFFICIAL $OUTPUT_PRERELEASE
 
 # Explicitely include prerelease legend cards from the official banlist.
 echo "# Prerelease Legends" >> $OUTPUT_PRERELEASE
 find . -type f -name '*.cdb' -exec sqlite3 {} \
-    "SELECT (datas.id || ' ' || 1 || ' --' || name) FROM datas INNER JOIN texts ON datas.id=texts.id WHERE ot == 0x100|0x200|0x400 ORDER BY datas.id" \; \
+    "SELECT (datas.id || ' ' || 3 || ' --' || name) FROM datas INNER JOIN texts ON datas.id=texts.id WHERE ot == 0x100|0x200|0x400 ORDER BY datas.id" \; \
     | tee -a $OUTPUT_PRERELEASE
-
-# Explicitely exclude prerelease legend cards from the official banlist.
-echo "# Prerelease Legends" >> $OUTPUT_OFFICIAL
-find . -type f -name '*.cdb' -exec sqlite3 {} \
-    "SELECT (datas.id || ' ' || -1 || ' --' || name) FROM datas INNER JOIN texts ON datas.id=texts.id WHERE ot == 0x100|0x200|0x400 ORDER BY datas.id" \; \
-    | tee -a $OUTPUT_OFFICIAL
 
